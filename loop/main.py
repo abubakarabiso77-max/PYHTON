8872d4e0352c463957c29b214236c09d1bf92bde
abubakarabiso77-max <abubakarabiso77@gmail.com>
2026-02-01 16:25:15 +0100

    main.py

---
+++ loop/main.py
@@ -0,0 +1,5 @@
+# pyhton program to print multiplication table from 23 to 1 to 10
+# loop from 1 to 10
+for i in range(1, 11):
+    # print multiplication table of 23 by i
+    print(f"23 x {i} = {23 * i}")
