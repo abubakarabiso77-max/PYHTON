02c2781bc48bb417a0b1df7deb693f846002a03e
abubakarabiso77-max <abubakarabiso77@gmail.com>
2026-02-21 16:36:54 +0100

    main.py

---
+++ loop/armstrong numbers/Functions/Turtle/pyhton 1/pyhton 2/object oriented programing/pyhton code/main.py
@@ -0,0 +1,14 @@
+class student :
+    grade = 9
+    name = "John"
+
+    def introduction(self):
+        print("hi i am a student")
+
+     def details(self):
+        print("my name is",self.name)
+        print("i study in Grade",self.grade)
+
+        ob = student()
+        ob introduction
+              ob details
